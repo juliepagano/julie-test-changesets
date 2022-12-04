2df0674952d1142faaa53e2c4d0295313ac8902a
Julie Pagano <julie.pagano@chronosphere.io>
2022-12-03 21:35:01 -0800

    Add date and time

--- ui/components/src/components/HelloWorld/HelloWorld.tsx
+++ ui/components/src/components/HelloWorld/HelloWorld.tsx
@@ -1,9 +1,14 @@
 import { HELLO_TARGET } from "@julie-test-changesets/core"
 
 export const HelloWorld = () => {
+  const time = new Date()
+
   return (
     <div>
-      Hello {HELLO_TARGET}!
+      <h1>Hello {HELLO_TARGET}!</h1>
+    <div>
+      It is {time.toLocaleTimeString()} on {time.toLocaleDateString()}.
+    </div>
     </div>
   )
 }
