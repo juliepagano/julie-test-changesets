934f75f2c3135e9a16328c404cc87391b35e09c1
Julie Pagano <julie.pagano@chronosphere.io>
2022-12-04 21:01:38 -0800

    Add time zone

--- ui/components/src/components/HelloWorld/HelloWorld.tsx
+++ ui/components/src/components/HelloWorld/HelloWorld.tsx
@@ -2,13 +2,13 @@ import { HELLO_TARGET } from "@julie-test-changesets/core"
 
 export const HelloWorld = () => {
   const time = new Date()
+  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
 
   return (
     <div>
       <h1>Hello {HELLO_TARGET}!</h1>
-    <div>
-      It is {time.toLocaleTimeString()} on {time.toLocaleDateString()}.
-    </div>
+      <p>It is {time.toLocaleTimeString()} on {time.toLocaleDateString()}.</p>
+      {timeZone && <p>Your time zone is: {timeZone}.</p>}
     </div>
   )
 }
