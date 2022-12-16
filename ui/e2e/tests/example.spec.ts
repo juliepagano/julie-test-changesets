8ecdfc31bd6242bdbbe877d7ac7050e205b21d88
Julie Pagano <julie.pagano@chronosphere.io>
2022-12-16 12:47:50 -0800

    Try localhost

--- ui/e2e/tests/example.spec.ts
+++ ui/e2e/tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('homepage has title', async ({ page }) => {
-  await page.goto('http://host.docker.internal:3000');
+  await page.goto('http://localhost:3000');
 
   await expect(page).toHaveScreenshot({
     mask: [page.locator('p')]
