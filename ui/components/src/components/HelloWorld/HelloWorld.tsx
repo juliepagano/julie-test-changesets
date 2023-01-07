6ff0a4eada9a31628a210f705be5fc1d75da21c0
Julie Pagano <julie.pagano@chronosphere.io>
2023-01-06 16:11:34 -0800

    change two (#41)

--- ui/components/src/components/HelloWorld/HelloWorld.tsx
+++ ui/components/src/components/HelloWorld/HelloWorld.tsx
@@ -11,7 +11,7 @@ export const HelloWorld = () => {
       {timeZone && <p>Your time zone is: {timeZone}.</p>}
       <strong>I like testing things! 😀</strong>
       <div>
-        Look at my cool new emojis: 👯‍♀️👯‍♀️👯‍♀️
+        Look at my cool new emojis: 🪩👯‍♀️👯‍♀️👯‍♀️🪩
       </div>
     </div>
   )
